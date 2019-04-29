c74ecb36ffe6642f059df16a624ebf5511edb985
khaled2095 <100082095@students.swinburne.edu.my>
2019-04-29 11:46:00 +0800

    Commiting changes
    New files

---
+++ PaintingsOnline/app/src/main/java/com/example/paintingsonline/Category/ArtistFragment.java
@@ -0,0 +1,4 @@
+package com.example.paintingsonline.Category;
+
+public class ArtistFragment {
+}
---
+++ PaintingsOnline/app/src/main/java/com/example/paintingsonline/Order/OrderActivity.java
@@ -0,0 +1,15 @@
+package com.example.paintingsonline.Order;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.paintingsonline.R;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
