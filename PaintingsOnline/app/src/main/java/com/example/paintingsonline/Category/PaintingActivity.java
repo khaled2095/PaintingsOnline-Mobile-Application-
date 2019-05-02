5f5048bc000dd1251acdb0836249c31329a21fa7
hamxaamir <100085748@students.swinburne.edu.my>
2019-05-02 14:53:07 +0800

    Show Paintings
    Showing paintings from each category

--- PaintingsOnline/app/src/main/java/com/example/paintingsonline/Category/PaintingActivity.java
+++ PaintingsOnline/app/src/main/java/com/example/paintingsonline/Category/PaintingActivity.java
@@ -1,15 +1,206 @@
 package com.example.paintingsonline.Category;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.paintingsonline.Model.Paintings;
 import com.example.paintingsonline.R;
+import com.example.paintingsonline.Utils.MySingleton;
+import com.nex3z.notificationbadge.NotificationBadge;
 
-public class PaintingActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaintingActivity extends AppCompatActivity implements PaintingsAdapterView.OnPaintListener
+{
+
+    private List<Paintings> paintingsList2;
+    private String URL = "https://jrnan.info/Painting/ShowPaintings.php?Category=";
+    public static CartDatabase cartd;
+    public static CartRepository cr;
+    NotificationBadge nb;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_painting);
+
+        nb = findViewById(R.id.badge);
+
+        /*setup backarrow for NAVIGATION */
+        ImageView backarrow = findViewById(R.id.backarrow);
+        backarrow.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                finish();
+            }
+        });
+
+        setupToolbar();
+
+        initDB();
+
+        loadPaintings();
+
+        paintingsList2 = new ArrayList<>();
+
+        JSONrequest();
+
+        updateCart();
+
     }
+
+    public void updateCart()
+    {
+        if (nb == null)
+        {
+            return;
+        }
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (cr.CountCartItems() == 0)
+                {
+                    nb.setVisibility(View.INVISIBLE);
+                }
+                else
+                {
+                    nb.setVisibility(View.VISIBLE);
+                    nb.setText(String.valueOf(cr.CountCartItems()));
+                }
+            }
+        });
+    }
+
+
+    private void initDB()
+    {
+        cartd = CartDatabase.getInstance(this);
+        cr = CartRepository.getInstance(CartDataSource.getInstance(cartd.cartDAO()));
+    }
+
+    private void setupToolbar()
+    {
+        Toolbar t1 = findViewById(R.id.paintingtoolbar);
+        setSupportActionBar(t1);
+
+        ImageView CartPage = findViewById(R.id.cart);
+
+        CartPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PaintingActivity.this, CartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu)
+//    {
+//        getMenuInflater().inflate(R.menu.cart, menu);
+//        View view = menu.findItem(R.id.cartmenu).getActionView();
+////        bg = view.findViewById(R.id.badge);
+//        return true;
+//    }
+
+    /*load paintings by Category*/
+    private void loadPaintings()
+    {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        int Category = sp.getInt("Selected",0);
+        URL += Category;
+    }
+
+    private void JSONrequest()
+    {
+        JsonArrayRequest request = new JsonArrayRequest(URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response)
+            {
+                JSONObject jsonObject = null;
+
+                for (int i=0; i < response.length(); i++)
+                {
+                    try
+                    {
+                        jsonObject = response.getJSONObject(i);
+
+                        int id = jsonObject.getInt("painting_id");
+                        String title = jsonObject.getString("painting_name");
+                        String image = jsonObject.getString("painting_url");
+                        int price = jsonObject.getInt("painting_price");
+                        String desc = jsonObject.getString("painting_description");
+
+                        Paintings paintings = new Paintings(id, title, desc, image, price);
+                        paintingsList2.add(paintings);
+                    }
+                    catch (JSONException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+
+                initrecyclerView(paintingsList2);
+            }
+        }, new Response.ErrorListener()
+        {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        MySingleton.getInstance(PaintingActivity.this).addToRequestQueue(request);
+    }
+
+
+
+    private void initrecyclerView(List<Paintings> paintingsList2)
+    {
+        RecyclerView recyclerView = findViewById(R.id.recycler_view_painting);
+        PaintingsAdapterView pav = new PaintingsAdapterView(this, paintingsList2 , this);
+        recyclerView.setAdapter(pav);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+//    @Override
+//    public void onPaintClick(int pos)
+//    {
+//        Intent intent = new Intent(this, PaintingDetail.class);
+//          intent.putExtra("img", paintingsList2.get(pos).getImage());
+//        intent.putExtra("paintname", paintingsList2.get(pos).getName());
+//        intent.putExtra("paintprice", paintingsList2.get(pos).getPrice());
+//        intent.putExtra("paintdesc", paintingsList2.get(pos).getDescription());
+//        intent.putExtra("paint", paintingsList2.get(pos));
+//        startActivity(intent);
+//    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateCart();
+    }
+
 }
--- PaintingsOnline/app/src/main/java/com/example/paintingsonline/Category/PaintingsAdapterView.java
+++ PaintingsOnline/app/src/main/java/com/example/paintingsonline/Category/PaintingsAdapterView.java
@@ -1,6 +1,134 @@
 package com.example.paintingsonline.Category;
 
-public class PaintingsAdapterView
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.paintingsonline.Model.Paintings;
+import com.example.paintingsonline.R;
+import com.google.gson.Gson;
+import com.nex3z.notificationbadge.NotificationBadge;
+
+import java.util.List;
+
+public class PaintingsAdapterView extends RecyclerView.Adapter<PaintingsAdapterView.PaintingHolder>
 {
+    private Context context;
+    private List<Paintings> plist;
+    private OnPaintListener monPaintListener;
+
+    public PaintingsAdapterView(Context context, List<Paintings> plist, OnPaintListener onPaintListener)
+    {
+        this.context = context;
+        this.plist = plist;
+        this.monPaintListener = onPaintListener;
+    }
+
+    @NonNull
+    @Override
+    public PaintingHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
+    {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.row_painting_item, viewGroup, false);
+        return new PaintingHolder(view, monPaintListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final PaintingHolder paintingHolder, final int i)
+    {
+        final Paintings p2 = plist.get(i);
+        paintingHolder.nameText.setText(p2.getName());
+        paintingHolder.priceText.setText(String.valueOf(p2.getPrice()));
+        paintingHolder.descriptionText.setText(p2.getDescription());
+
+        Glide.with(context).load(p2.getImage()).into(paintingHolder.i2);
+
+
+
+        paintingHolder.cartbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+
+                //Add to SQLite Room Persistance
+                //Create New Checkout Item
+                try {
+
+                    Cart cart = new Cart();
+                    cart.paintingid = p2.getId();
+                    cart.paintingimg = p2.getImage();
+                    cart.paintingname = p2.getName();
+                    cart.price = p2.getPrice();
+                    cart.qty = 1;
+
+                    CartDatabase cartd = CartDatabase.getInstance(context);
+                    CartRepository cartRepository = CartRepository.getInstance(CartDataSource.getInstance(cartd.cartDAO()));
+                    cartRepository.insertToCart(cart);
+
+                    Log.d("cart", new Gson().toJson(cart));
+                    Toast.makeText(context, "Saved Item to Checkout", Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception ex)
+                {
+                    Toast.makeText(context, ex.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return plist.size();
+    }
+
+    /*View Holder*/
+    class PaintingHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+    {
+        ImageView i2;
+        TextView descriptionText, priceText, nameText;
+        Button cartbtn;
+        NotificationBadge nb;
+        OnPaintListener onPaintListener;
+
+        public PaintingHolder(@NonNull View itemView, OnPaintListener onPaintListener)
+        {
+            super(itemView);
+            i2 = itemView.findViewById(R.id.imgpaintings2);
+            nameText = itemView.findViewById(R.id.painting_Name2);
+            priceText = itemView.findViewById(R.id.price2);
+            cartbtn = itemView.findViewById(R.id.cartbtn);
+            descriptionText = itemView.findViewById(R.id.paintDesc);
+            cartbtn = itemView.findViewById(R.id.cartbtn);
+            this.onPaintListener = onPaintListener;
+
+            itemView.setOnClickListener(this);
+
+        }
+
+
+        @Override
+        public void onClick(View view)
+        {
+            onPaintListener.onPaintClick(getAdapterPosition());
+        }
+    }
+
+    public interface OnPaintListener
+    {
+        void onPaintClick(int pos);
+    }
 
 }
