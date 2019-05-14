fe65acfcde6782df95b87e143e36f13ae1169e4b
khaled2095 <100082095@students.swinburne.edu.my>
2019-05-14 23:23:10 +0800

    Room model class

---
+++ PaintingsOnline/app/src/main/java/com/example/paintingsonline/Model/Room.java
@@ -0,0 +1,41 @@
+package com.example.paintingsonline.Model;
+
+public class Room
+{
+    private int roomid;
+    private String roomName;
+    private String roomimageURL;
+
+
+    public Room(int roomid, String roomName, String roomimageURL)
+    {
+        this.roomid = roomid;
+        this.roomName = roomName;
+        this.roomimageURL = roomimageURL;
+    }
+
+
+    public int getRoomid() {
+        return roomid;
+    }
+
+    public void setRoomid(int roomid) {
+        this.roomid = roomid;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getRoomimageURL() {
+        return roomimageURL;
+    }
+
+    public void setRoomimageURL(String roomimageURL) {
+        this.roomimageURL = roomimageURL;
+    }
+}
