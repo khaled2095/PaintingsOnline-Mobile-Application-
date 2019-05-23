be67d16882a63394ed94db8213f47a763164c8bf
100034601 <dr.mm94@gmail.com>
2019-05-23 21:08:26 +0800

    Restructured the Loading of Data

---
+++ iOS/PaintingsOnline/PaintingsOnline/CartItem.swift
@@ -0,0 +1,20 @@
+//
+//  CartItem.swift
+//  PaintingsOnline
+//
+//  Created by Mohammed Alharthy on 18/5/19.
+//  Copyright © 2019 Mohammed Alharthy. All rights reserved.
+//
+
+import UIKit
+
+class CartItem: NSObject {
+    var Cartname = ""
+    var CartPrice = ""
+    var CartArtist = ""
+    var Cartdescription = ""
+    var CartID = ""
+    var CartUrl = ""
+    var Quantity = 0
+    var MaxQuantity = 0
+}
